451a3b671a64f0d9d8dac062d2648562e966af8b
MininDS <MininDS178@gmail.com>
2025-02-13 17:27:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Gatling Demo-store Code Repository
+============================================
+
+
---
+++ src/test/java/gatlingdemostore/pageobjects/CmsPage.java
@@ -0,0 +1,23 @@
+package gatlingdemostore.pageobjects;
+
+import io.gatling.javaapi.core.ChainBuilder;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.http;
+
+public final class CmsPage {
+
+    public static final ChainBuilder homepage =
+            exec(
+                    http("Load Home Page")
+                            .get("/")
+                            .check(regex("<title>Gatling Demo-Store</title>").exists())
+                            .check(css("#_csrf", "content").saveAs("csrfValue")));
+
+    public static final ChainBuilder aboutUs =
+            exec(
+                    http("Load About Us Page")
+                            .get("/about-us")
+                            .check(substring("About Us")));
+
+}
